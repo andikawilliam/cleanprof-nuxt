34e2231c71164441de27d36386250f052da2d15b
vandercoder <andikawilliam@outlook.com>
2020-12-29 15:36:33 +0700

    add list in home

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,9 +34,27 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    '@nuxt/content'
   ],
+
+  content: {
+    // Options
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
+  },
+
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./content/blog').map(file => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./content/blog/${file}`),
+        };
+      });
+    },
+  },
 }
