5e952093656b57b581e68b28861b1c64a60ee114
vandercoder <andikawilliam@outlook.com>
2020-12-31 21:37:18 +0700

    configure Retailer page with retailer collections

--- pages/retailers.vue
+++ pages/retailers.vue
@@ -9,25 +9,45 @@
         <p class="font-univers font-bold text-center text-3xl sm:text-left sm:text-5xl ">
           Retailers
         </p>
-        <div class="font-omnes text-center text-gray-700 mt-10 sm:text-left">
+        <div 
+          class="font-omnes text-center text-gray-700 mt-10 sm:text-left"
+          v-for="retailer in retailers"
+          v-bind:key="retailer.slug"
+        >
           <a href="https://www.tokopedia.com/klikdentalindo">
             <img 
               class="w-3/4 mx-auto sm:mx-0 md:w-1/3 transform hover:scale-105 transition-transform duration-500"
-              :src="require(`~/assets/resources/logo/klik-dental.png`)" 
+              v-bind:src="retailer.logo" 
             >
-            <p class="font-black text-3xl sm:text-4xl mt-2">Klik Dental Sejahtera</p>
+            <p class="font-black text-3xl sm:text-4xl mt-2">
+              {{ retailer.retailer }}
+            </p>
           </a>
           <div class="lg:flex text-left text-lg sm:text-2xl py-4 px-6">
             <div class="lg:w-1/2 grid gap-2 grid-cols-5 mb-4">
-              <p class="font-bold col-span-2 lg:col-span-1">Alamat:</p>
-              <p class="col-span-3 lg:col-span-4">Jalan Selatan Raya No. 1 CBD Pluit Ruko B-2,, RT.23/RW.8, Penjaringan, North Jakarta City, Jakarta 14440</p>
+              <p class="font-bold col-span-2 lg:col-span-1">
+                Alamat:
+              </p>
+              <p class="col-span-3 lg:col-span-4">
+                {{ retailer.alamat }}
+              </p>
             </div>
 
             <div class="lg:w-1/2 grid gap-4 grid-cols-5 lg:ml-24">
-              <p class="font-bold col-span-2 lg:col-span-1">Telepon:</p>
-              <p class="col-span-3 lg:col-span-4">(021) 919191</p>
-              <p class="font-bold col-span-2 lg:col-span-1">Website:</p>
-              <p class="col-span-3 lg:col-span-4"><a href="https://www.klikdental.co.id/">www.klikdental.co.id</a></p>
+              <p class="font-bold col-span-2 lg:col-span-1">
+                Telepon:
+              </p>
+              <p class="col-span-3 lg:col-span-4">
+                {{ retailer.telepon }}
+              </p>
+              <p class="font-bold col-span-2 lg:col-span-1">
+                Website:
+              </p>
+              <p class="col-span-3 lg:col-span-4">
+                <a :href="goToWeb(retailer.website)">
+                  {{ retailer.website }}
+                </a>
+              </p>
             </div>
           </div>
         </div>
@@ -45,10 +65,17 @@ export default {
       background: "underwater.jpg"
     }
   },
+  methods: {
+    goToWeb: function(link) {
+      return "http://" + link;
+    }
+  },
+
   async asyncData({ $content }) {
+    const retailers = await $content("retailers").fetch();
     const contact = await $content("contact").fetch();
     return {
-      contact
+      retailers, contact
     };
   },
 }
